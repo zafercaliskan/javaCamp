bf93df12aa93de985cfa223ca7855ebe87603211
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-05-02 00:13:25 +0300

    add 4th day project files (#2)

---
+++ interfaces/src/interfaces/Customer.java
@@ -0,0 +1,40 @@
+package interfaces;
+
+public class Customer {
+	int id;
+	String firstName;
+	String lastName;
+	
+	public Customer() {
+	}
+
+	public Customer(int id, String firstName, String lastName) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}	
+}
---
+++ interfaces/src/interfaces/SmsLogger.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+public class SmsLogger implements Logger {
+
+	@Override
+	public void log(String message) {
+		System.out.println("Sms gönderildi. " + message);
+		
+	}
+}
