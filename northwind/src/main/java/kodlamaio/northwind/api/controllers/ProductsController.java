1857e56f32ce87c34c29ae934806c1fcad631417
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-05-30 22:42:50 +0300

    (Java & React) 8th Day (#3)
    * Episode 1
    
    * Episode 2

--- northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
@@ -45,5 +46,10 @@ public class ProductsController {
 	@PostMapping("/add")
 	public Result add(@RequestBody Product product) {
 		return this.productService.add(product);
+	}
+	
+	@GetMapping("/getByProductName")
+	public DataResult<Product> getByProductName(@RequestParam String productName){
+		return this.productService.getByProductName(productName);
 	}
 }
--- northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -9,6 +9,21 @@ import kodlamaio.northwind.entities.concretes.Product;
 public interface ProductService { //controllerların kullanacağı operasyonları yazarız. 
 	//İş katmanında sadece ve sadece iş kodları yazılır. 
 	//ürün eklerken bir kuralımız varsa örneğin hepsiburada üretici ilk ay kendi ürününden fazla 100 tane ekleyebilir.
-	DataResult<List<Product>> getAll(); //Burada success yada error olduğunu belirtmedik.
-	Result add(Product product);
+      
+	DataResult<List<Product>> getAll(); //Burada success yada error olduğunu belirtmedik.   
+	Result add(Product product);     
+      
+	DataResult<Product> getByProductName(String productName);    
+      
+	DataResult<Product> getByProductNameAndCategoryId(String productName, int categoryId);	  
+      
+	DataResult<List<Product>> getByProductNameOrCategoryId(String productName, int categoryId);	  
+      
+	DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories);	  
+      
+	DataResult<List<Product>> getByProductNameContains(String productName);	  
+      
+	DataResult<List<Product>> getByProductNameStartsWith(String productName);
+      
+	DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId);
 }
